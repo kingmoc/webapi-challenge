48bc994f0f31b62e0e7b9a40fc02595bcbe7b8f5
kingmoc <joe.c.a.garcia@gmail.com>
2019-08-09 10:36:18 -0500

    Data Setup

--- server.js
+++ server.js
@@ -1,5 +1,53 @@
 const express = require('express')
 
+let people = [
+    {
+        id: 1,
+        name: "Joseph",
+        chores: [
+            {
+                id: 1,
+                description: "Clean House",
+                notes: "",
+                assignedTo: 1,
+                completed: false
+            },
+            {
+                id: 2,
+                description: "Mow the Lawn",
+                notes: "",
+                assignedTo: 1,
+                completed: false
+            }
+        ]
+    },
+    {   id: 2,
+        name: "Kate",
+        chores: [
+            {
+                id: 1,
+                description: "Write Daily Journal",
+                notes: "",
+                assignedTo: 2,
+                completed: false
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Ajalina",
+        chores: [
+            {
+                id: 1,
+                description: "Do Homework",
+                notes: "",
+                assignedTo: 3,
+                completed: false
+            }
+        ]
+    }
+]
+
 
 const server = express();
 server.use(express.json())
