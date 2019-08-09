1a8f4253b2a80689b94b0076fa78231e6b368f22
kingmoc <joe.c.a.garcia@gmail.com>
2019-08-09 10:22:34 -0500

    Init Setup - Server Running

---
+++ index.js
@@ -0,0 +1,13 @@
+// require('dotenv').config()
+
+const server = require("./server.js");
+
+
+
+const port = process.env.PORT || 5000;
+const greeting = "Hi, Mr. Garcia!";
+server.listen(port, () => {
+  console.log(
+    `\n*** ${greeting} Server Running on http://localhost:${port} ***\n`
+  );
+});
